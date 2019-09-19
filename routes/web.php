936749eef4bcb29228b93dc9eb5d8420ac62e33d
yuanwenbo <1291173817@qq.com>
2019-09-20 03:20:32 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+// 注册页面
+Route::get('signup', 'UsersController@create')->name('signup');
