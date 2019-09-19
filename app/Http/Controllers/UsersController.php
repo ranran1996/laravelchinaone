c9fec1d1301e40ad48854c73df392783bb112222
yuanwenbo <1291173817@qq.com>
2019-09-20 05:11:22 +0800

    add User

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -10,5 +11,13 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    // User是对应的User模型，$user会匹配路由片段中的{user}，这样Laravel会自动注入与请求 URI 中传入的 ID 对应的用户模型实例
+    // 路由地址是/users/{user}对应这个控制器
+    // 如果数据库中找不到对应的模型实例，会自动生成 HTTP 404 响应
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
