91fceeb633a08da687fa75f031e4d585c9f2ec46
yuanwenbo <690369385@qq.com>
2019-09-19 21:18:04 +0000

    add User route

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 // 注册页面
 Route::get('signup', 'UsersController@create')->name('signup');
+
+Route::resource('users', 'UsersController');
