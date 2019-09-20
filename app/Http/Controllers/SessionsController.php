99afa9a7e01933515bfdb3f6052a49a0c0650210
yuanwenbo <1291173817@qq.com>
2019-09-20 23:19:58 +0800

    创建登陆会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    //显示登陆页面
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    //登陆操作
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // 登陆成功相关操作
+            session()->flash('success', '欢迎回来！');
+            // Auth::user() 获取当前登陆用户的信息,相当于把用户信息全带过去，并直接跳转到/users/指定id地址
+            // dd(Auth::user());
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            // 登陆失败相关操作
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+
+            return redirect()->back()->withInput();
+        }
+    }
+}
